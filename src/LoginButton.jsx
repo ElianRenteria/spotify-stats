1764b019274aaaf3e354ce5a67917e0376136c3a
elianrenteria <elianrenteriadevelopment@gmail.com>
2025-03-08 12:46:19 -0800

    build 3

--- src/LoginButton.jsx
+++ src/LoginButton.jsx
@@ -1,5 +1,5 @@
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-const REDIRECT_URI = "https://elianrenteria.github.io/spotify-stats/callback"; // Adjust when deploying
+const REDIRECT_URI = "https://elianrenteria.github.io/spotify-stats"; // Adjust when deploying
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token"; // Implicit Grant Flow (or use Authorization Code Flow with backend)
 const SCOPES = "user-top-read"; // Request user's top artists
@@ -9,7 +9,6 @@ const LoginButton = () => {
     const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=${RESPONSE_TYPE}&scope=${encodeURIComponent(SCOPES)}`;
     window.location.href = authUrl; // Redirect to Spotify login
   };
-
   return (
         <div className='flex items-center justify-center h-full w-full'>
             <button className='btn btn-primary mb-30' onClick={login}>Login with Spotify</button>
